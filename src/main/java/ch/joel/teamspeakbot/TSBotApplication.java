8c5dcd1e1379c9d02474c030db3f99a2c4d04919
Joel Sauvain <joel.sauvain@hotmail.com>
2017-09-10 15:09:39 +0200

    api update

--- src/main/java/ch/joel/teamspeakbot/TSBotApplication.java
+++ src/main/java/ch/joel/teamspeakbot/TSBotApplication.java
@@ -1,15 +1,17 @@
 package ch.joel.teamspeakbot;
 
-import ch.joel.teamspeakbot.config.ConfigLoader;
 import ch.joel.teamspeakbot.listeners.AfkListener;
 import ch.joel.teamspeakbot.listeners.ClientJoinListener;
 import ch.joel.teamspeakbot.listeners.ForbiddenNamesListener;
 import ch.joel.teamspeakbot.listeners.WroteMeListener;
+import ch.jooel.config.YamlConfigurationFactory;
 import com.github.theholywaffle.teamspeak3.TS3Api;
 import com.github.theholywaffle.teamspeak3.TS3ApiAsync;
 import com.github.theholywaffle.teamspeak3.TS3Config;
 import com.github.theholywaffle.teamspeak3.TS3Query;
 import com.github.theholywaffle.teamspeak3.api.wrapper.Client;
+
+import java.io.File;
 
 public class TSBotApplication {
 
@@ -23,7 +25,7 @@ public class TSBotApplication {
 	}
 
 	private void start() {
-		configuration = new ConfigLoader().load();
+		loadConfig();
 		setupTeamSpeakConnection();
 		welcomeUsers();
 
@@ -32,6 +34,14 @@ public class TSBotApplication {
 		new AfkListener(apiAsync, configuration).run();
 		new ForbiddenNamesListener(apiAsync, configuration).run();
 		System.out.println("Bot started");
+	}
+
+	private void loadConfig() {
+		YamlConfigurationFactory factory = new YamlConfigurationFactory();
+		if (!new File("config.yml").exists())
+			factory.copyFromResource("/config.yml", "config.yml");
+		configuration = factory.load(TSBOTConfiguration.class, "config.yml");
+		System.out.println(configuration);
 	}
 
 	private void setupTeamSpeakConnection() {
@@ -43,7 +53,6 @@ public class TSBotApplication {
 
 		api = query.getApi();
 		apiAsync = query.getAsyncApi();
-		// tsbot RRqMIeJm
 		api.login(configuration.getServer().getUsername(), configuration.getServer().getPassword());
 		api.selectVirtualServerById(1);
 		api.setNickname(configuration.getNickname());
